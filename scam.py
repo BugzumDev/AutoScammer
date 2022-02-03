34cbe4bf9b805567648f4c8fa7882fba2d46bc05
BugzumDev <bugzumprogramming@gmail.com>
2022-02-03 19:42:28 +0100

    Testing...

--- scam.py
+++ scam.py
@@ -1,1 +1,48 @@
-# ree
+# AutoScammer v:1.0
+# Made by: BugzumDev
+
+# I am NOT responsible for any damage
+# Please don't go out and scam people with this
+
+# Libaries
+from fbchat import Client
+from fbchat.models import *
+
+# log in
+client = Client("<email>", "<password>")
+
+# Thread setup
+thread_id = "1234567890"
+thread_type = ThreadType.GROUP
+
+# basic runtime information
+scamname = "<scamName>"
+scamtemplate = 1
+language = "hu"
+
+# Scam template 1
+scamdevice = "<scamDevice>"
+scamend = "-t"
+
+# Scamtemplate 2
+scamvalue = "<scamAmount>"
+
+# Just some basic info
+scamlink = "<scamLink>"
+scamemoji = "🤑📱"
+
+# Scam templates
+if scamtemplate == 1:
+    if language == "hu":
+        scammsg = "Gratulálunk " + scamname + "! Ön nyert egy új " + scamdevice + scamend + "! Az ezközt megrendelheted ingyenesen itt: " + scamlink + " " + scamemoji
+    elif language == "en":
+        scammsg = "Congratulations " + scamname + "! You just won a " + scamdevice + scamend + "! You can redeem the device here: " + scamlink + " " + scamemoji
+elif scamtemplate == 2:
+    if language == "hu":
+        scammsg = scamname + ", önnek még van " + scamvalue + " összege az amazon számláján ami hamarosan lejár! Kattincson ide az összeg felhasználásához: " + scamvalue
+    elif language == "en":
+        scammsg = scamname + ", you still have a " + scamvalue + " gift card that is going to expire! You can use the card here: " + scamvalue
+
+
+# Send message
+client.send(Message(text=scammsg), thread_id=thread_id, thread_type=thread_type)
